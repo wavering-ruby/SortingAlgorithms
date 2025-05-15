db71ab80a20a789ecf611093b68203ead7d86128
Mateus Gabriel <82602817+wavering-ruby@users.noreply.github.com>
2025-05-14 22:13:36 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,63 @@
 using namespace std;
 using namespace std::chrono;
 
+// A utility function to get maximum
+// value in arr[]
+int getMax(int arr[], int n){ // Radix sort
+    int mx = arr[0];
+    for (int i = 1; i < n; i++)
+        if (arr[i] > mx)
+            mx = arr[i];
+    return mx;
+}
+
+// A function to do counting sort of arr[]
+// according to the digit
+// represented by exp.
+void countSort(int arr[], int n, int exp){ // Radix sort
+    // Output array
+    int output[n];
+    int i, count[10] = { 0 };
+
+    // Store count of occurrences
+    // in count[]
+    for (i = 0; i < n; i++)
+        count[(arr[i] / exp) % 10]++;
+
+    // Change count[i] so that count[i]
+    // now contains actual position
+    // of this digit in output[]
+    for (i = 1; i < 10; i++)
+        count[i] += count[i - 1];
+
+    // Build the output array
+    for (i = n - 1; i >= 0; i--) {
+        output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+        count[(arr[i] / exp) % 10]--;
+    }
+
+    // Copy the output array to arr[],
+    // so that arr[] now contains sorted
+    // numbers according to current digit
+    for (i = 0; i < n; i++)
+        arr[i] = output[i];
+}
+
+// The main function to that sorts arr[]
+// of size n using Radix Sort
+void radixsort(int arr[], int n){ // radixsort
+    // Find the maximum number to
+    // know number of digits
+    int m = getMax(arr, n);
+
+    // Do counting sort for every digit.
+    // Note that instead of passing digit
+    // number, exp is passed. exp is 10^i
+    // where i is current digit number
+    for (int exp = 1; m / exp > 0; exp *= 10)
+        countSort(arr, n, exp);
+}
+
 void insertionSort(int arr[], int n){
     for (int i = 1; i < n; ++i) {
         int key = arr[i];
@@ -86,7 +143,18 @@ int main() {
     start = high_resolution_clock::now();
   	
   	// Função do InsertionSort
-    insertionSort(array, n);
+    insertionSort(array1, n);
+    
+    end = high_resolution_clock::now();
+    
+    cout << duration_cast<nanoseconds>(end - start).count() << " nanoseconds" << endl;
+    
+    int array2[] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+    
+    start = high_resolution_clock::now();
+  	
+  	// Função do InsertionSort
+    radixsort(array2, n);
     
     end = high_resolution_clock::now();
     
